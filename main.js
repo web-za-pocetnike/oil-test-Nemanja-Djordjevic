551c2418697300f3d71e7ba50a6c61a1a88544bf
Radisa Bogdanovic <73960420+radisa-bogdanovic@users.noreply.github.com>
2021-03-17 21:05:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function zatvoriMeni (){
+ const zatvori = document.querySelector(".iskljuci");  
+ zatvori.classList.toggle("active");
+ const navigacija = document.querySelector(".navigacija");
+  
+ navigacija.classList.toggle("active");
+}
